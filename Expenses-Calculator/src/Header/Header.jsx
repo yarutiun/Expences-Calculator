0004681d30ab5b5bba0343af4ca17f7e74cddfc6
yarutiun <y.arutiunofff@gmail.com>
2024-11-05 11:52:26 +0100

    basic layout done

--- Expenses-Calculator/src/Header/Header.jsx
+++ Expenses-Calculator/src/Header/Header.jsx
@@ -1,6 +1,7 @@
 import style from './Header.module.css';
 import menu from '../assets/menu.png';
 import user from '../assets/user.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
         <header className={style.mainContainer}>
           <button><img src={menu} alt='menu'/></button>
           <h1>Where&apos;s the money gone?</h1>
-          <button><img src={user} alt='user'/></button>
+          <Link to='/profile'><button><img src={user} alt='user'/></button></Link>
         </header>
     </div>
   )
--- Expenses-Calculator/src/main.jsx
+++ Expenses-Calculator/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/profile",
+    element: <h1>Profile</h1>,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
